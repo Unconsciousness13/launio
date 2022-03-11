2b6ea885ae4102c65baf0235ae5d04b27f01afd1
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-11 12:33:41 +0100

    Project

--- launio/club/urls.py
+++ launio/club/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 
-from launio.club.views import show_index, show_contact, register_user, trainers, directors, gymnasts, login_user, \
-    UserLogoutView
+# urlpatterns = (
+#     path('', show_index, name='show index'),
+#     path('contact/', show_contact, name='show contact'),
+#     path('register/', register_user, name='register page'),
+#     path('login/', login_user, name='login page'),
+#     path('trainers/', trainers, name='trainers page'),
+#     path('directors/', directors, name='directors page'),
+#     path('gymnasts/', gymnasts, name='gymnasts page'),
+#     path('logout/', UserLogoutView.as_view(), name='logout user'),
+# )
+from launio.club.views import HomeView, RegisterView
+
+# from launio.club.views import show_index, show_contact, register_user, trainers, directors, gymnasts, login_user, \
+#     UserLogoutView
 
 urlpatterns = (
-    path('', show_index, name='show index'),
-    path('contact/', show_contact, name='show contact'),
-    path('register/', register_user, name='register page'),
-    path('login/', login_user, name='login page'),
-    path('trainers/', trainers, name='trainers page'),
-    path('directors/', directors, name='directors page'),
-    path('gymnasts/', gymnasts, name='gymnasts page'),
-    path('logout/', UserLogoutView.as_view(), name='logout user'),
+    path('', HomeView.as_view(), name='show index'),
+    path('register/', RegisterView.as_view(), name='register page')
 )
--- launio/club/views.py
+++ launio/club/views.py
@@ -1,104 +1,104 @@
-from django.contrib.auth import forms as auth_forms
-from django.contrib.auth import views as auth_views
-from django.shortcuts import render, redirect
 from django.views import generic as views
 
-from launio.club.forms import CreateProfileForm
-from launio.club.models import Profile
 
-
-def get_profile():
-    profiles = Profile.objects.all()
-    if profiles:
-        return profiles[0]
-    return None
-
-
-def show_index(request):
-    profile = get_profile()
-    # if not profile:
-    #     return redirect('show index')
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'home.html', context)
-
-
-def show_contact(request):
-    profile = get_profile()
-    if not profile:
-        return redirect('register page')
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'contact.html', context)
-
-
-def register_user(request):
-    if request.method == 'POST':
-        form = CreateProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('show index')
-
-    else:
-        form = CreateProfileForm()
-
-    context = {
-        'form': form,
-        'no_profile': True,
-    }
-    return render(request, 'register.html', context)
-
-
-def login_user(request):
-    return render(request, 'login.html')
-
-
-# def contact_page(request):
-
-def trainers(request):
-    profile = get_profile()
-    if not profile:
-        return redirect('register page')
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'trainers.html', context)
-
-
-def gymnasts(request):
-    profile = get_profile()
-    if not profile:
-        return redirect('register page')
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'gymnasts.html', context)
-
-
-def directors(request):
-    profile = get_profile()
-    if not profile:
-        return redirect('register page')
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'directors.html', context)
-
-
-# Classes
-
-
-class UserLogoutView(auth_views.LogoutView):
-    pass
+# def get_profile():
+#     profiles = Profile.objects.all()
+#     if profiles:
+#         return profiles[0]
+#     return None
+#
+#
+# def show_index(request):
+#     profile = get_profile()
+#     # if not profile:
+#     #     return redirect('show index')
+#
+#     context = {
+#         'profile': profile,
+#     }
+#     return render(request, 'home.html', context)
+#
+#
+# def show_contact(request):
+#     profile = get_profile()
+#     if not profile:
+#         return redirect('register page')
+#
+#     context = {
+#         'profile': profile,
+#     }
+#     return render(request, 'contact.html', context)
+#
+#
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = CreateProfileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('show index')
+#
+#     else:
+#         form = CreateProfileForm()
+#
+#     context = {
+#         'form': form,
+#         'no_profile': True,
+#     }
+#     return render(request, 'register.html', context)
+#
+#
+# def login_user(request):
+#     return render(request, 'login.html')
+#
+#
+# # def contact_page(request):
+#
+# def trainers(request):
+#     profile = get_profile()
+#     if not profile:
+#         return redirect('register page')
+#
+#     context = {
+#         'profile': profile,
+#     }
+#     return render(request, 'trainers.html', context)
+#
+#
+# def gymnasts(request):
+#     profile = get_profile()
+#     if not profile:
+#         return redirect('register page')
+#
+#     context = {
+#         'profile': profile,
+#     }
+#     return render(request, 'gymnasts.html', context)
+#
+#
+# def directors(request):
+#     profile = get_profile()
+#     if not profile:
+#         return redirect('register page')
+#
+#     context = {
+#         'profile': profile,
+#     }
+#     return render(request, 'directors.html', context)
+#
+#
+# # Classes
+#
+#
+# class UserLogoutView(auth_views.LogoutView):
+#     pass
 #
 #
 # class UserRegistrationView(views.CreateView):
 #     form_class = auth_forms.UserCreationForm
 #     template_name = 'register.html'
+class HomeView(views.TemplateView):
+    template_name = 'home.html'
+
+
+class RegisterView(views.CreateView):
+    template_name = 'register.html'
