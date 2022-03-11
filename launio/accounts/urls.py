111b22d97cb21e23aa5961f18de15315646997f0
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-11 14:09:44 +0100

    Project

--- launio/accounts/urls.py
+++ launio/accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from launio.accounts.views import RegisterView
 
 urlpatterns = (
-    path('register/', RegisterView.as_view(), name='register page')
+    path('register/', RegisterView.as_view(), name='register page'),
 )
