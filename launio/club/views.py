902e88ea2d1e88c93c1dc6a447eba86c68711e92
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-15 14:39:27 +0100

    Project

--- launio/club/views.py
+++ launio/club/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views import generic as views
 
 # def get_profile():
@@ -99,6 +100,16 @@ from django.views import generic as views
 
 class HomeView(views.TemplateView):
     template_name = 'home.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['hide_additional_nav_items'] = True
+    #     return context
+    #
+    # def dispatch(self, request, *args, **kwargs):
+    #     if request.user.is_autenticated:
+    #         return redirect('dashboard')
+    #     return super().dispatch(request, *args, **kwargs)
 
 
 
