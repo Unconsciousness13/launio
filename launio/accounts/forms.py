9e106ffc66f121e0f010609ccc5a91b95a3b1aec
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-18 12:15:28 +0100

    Project

--- launio/accounts/forms.py
+++ launio/accounts/forms.py
@@ -1,28 +1,39 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 user = get_user_model()
 
 
+class UniqueUserEmailField(forms.EmailField):
+
+    def validate(self, value):
+        super(forms.EmailField, self).validate(value)
+        if User.objects.filter(email=value).exists():
+            raise forms.ValidationError("Usuario con este correo electronico ya existe")
+        # try:
+        #     User.objects.get(email=value)
+        #     raise forms.ValidationError("Email already exists")
+        # except User.MultipleObjectsReturned:
+        #     raise forms.ValidationError("Email already exists")
+
+
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='Campo obligatorio')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Campo obligatorio')
-    email = forms.EmailField(max_length=254, help_text='Correo electronico tiene que ser valido')
-    username = forms.CharField(max_length=30, required=True, help_text='Campo obligatorio')
+    first_name = forms.CharField(max_length=30, required=True, label='Nombre')
+    last_name = forms.CharField(max_length=30, required=True, label="Apellido")
+    email = UniqueUserEmailField(required=True, label='Correo electronico')
+    username = forms.CharField(max_length=30, required=True, label='Usuario')
 
     class Meta:
         model = get_user_model()
-        fields = ['username',
+        fields = ('username',
                   'first_name',
                   'last_name',
                   'email',
                   'password1',
-                  'password2', ]
+                  'password2',)
 
-    # def clean(self, *args, **kwargs):
-    #     email = self.cleaned_data.get('email')
-    #     email_user = user.objects.filter(email=email)
-    #     if email_user.exist():
-    #         raise forms.ValidationError('User with this  email already exist')
-    #     return super(UserCreationForm, self).clean(*args, **kwargs)
+
+#
+# class LoginForm(UserLoginForm):
--- launio/club/forms.py
+++ launio/club/forms.py
@@ -2,41 +2,6 @@ from django import forms
 
 
 
-
-
-#
-# class CreateProfileForm(forms.ModelForm):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#
-#     class Meta:
-#         model = Profile
-#         fields = ('username', 'email', 'age')
-#
-#         labels = {
-#             'username': 'Username',
-#             'email': 'Email',
-#             'age': 'Age',
-#         }
-#
-#         widgets = {
-#             "username": forms.TextInput(attrs={
-#                 "type": "text", "name": "username", "id": "first_name",
-#                 "placeholder": "Username"
-#             }),
-#             "email": forms.EmailInput(attrs={
-#                 "type": "text", "name": "email", "id": "email",
-#                 "placeholder": "Email"
-#             }),
-#
-#             "age": forms.NumberInput(attrs={
-#                 "type": "number", "name": "age", "id": "age",
-#                 "placeholder": "Age", "min": "0"
-#             }),
-#
-#         }
-#
-#
 # class CreateContactForm(forms.ModelForm):
 #     def __init__(self, *args, **kwargs):
 #         super().__init__(*args, **kwargs)
