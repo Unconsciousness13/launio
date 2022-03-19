519c04880cb2cec6da2952880ac9bf45803708de
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-19 16:33:13 +0100

    Project

--- launio/club/urls.py
+++ launio/club/urls.py
@@ -1,15 +1,5 @@
 from django.urls import path
 
-# urlpatterns = (
-#     path('', show_index, name='show index'),
-#     path('contact/', show_contact, name='show contact'),
-#     path('register/', register_user, name='register page'),
-#     path('login/', login_user, name='login page'),
-#     path('trainers/', trainers, name='trainers page'),
-#     path('directors/', directors, name='directors page'),
-#     path('gymnasts/', gymnasts, name='gymnasts page'),
-#     path('logout/', UserLogoutView.as_view(), name='logout user'),
-# )
 from launio.club.views import HomeView, EntrenadorasView, GymnastsView, AddGymnastView, AddTrainerView
 
 urlpatterns = (
