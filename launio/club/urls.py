d740d791aca3da0620174a5bbe09b97c43870631
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-03-21 14:11:41 +0100

    Project

--- launio/club/urls.py
+++ launio/club/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from launio.club.views import HomeView, EntrenadorasView, GymnastsView, AddGymnastView, AddTrainerView
 
 urlpatterns = (
     path('', HomeView.as_view(), name='show index'),
     path('trainers/', EntrenadorasView.as_view(), name='show trainers'),
-    path('gymnasts/', GymnastsView.as_view, name="show gymnast"),
-    path('addgymnast/', AddGymnastView.as_view, name='add gymnast'),
-    path('addtrainer/', AddTrainerView.as_view, name='add trainer'),
+    path('gymnasts/', GymnastsView.as_view(), name="show gymnast"),
+    path('addgymnast/', AddGymnastView.as_view(), name='add gymnast'),
+    path('addtrainer/', AddTrainerView.as_view(), name='add trainer'),
+
 )
--- static/js/script.js
+++ static/js/script.js
@@ -12,4 +12,5 @@
     else{
     	nav.removeAttribute("style");
     }
- } 
+ }
+ 
