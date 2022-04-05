b4ed875017a1a3762c97defe2487f41edf50f6b1
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-04-05 09:56:45 +0200

    Fixes

--- launio/settings.py
+++ launio/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = 'home.html'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pakotestpako@gmail.com'
