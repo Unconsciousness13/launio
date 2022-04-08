f0f9e7146e0689536c94cfd1f47fdaa8f5d41c26
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-04-08 12:47:06 +0200

    changed db data

--- launio/settings.py
+++ launio/settings.py
@@ -136,11 +136,16 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 )
+# STATICFILES_DIRS = (
+#     BASE_DIR / "static",
+# )
 # STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'mediafiles/'
