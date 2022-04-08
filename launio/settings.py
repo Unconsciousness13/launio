bb46783fc674840fe4aba91e1c7e50b102737493
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-04-08 11:00:36 +0200

    adding STATIC_URL

--- launio/settings.py
+++ launio/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / "static",
 )
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
