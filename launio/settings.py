8029372cd1d01803d447051c40324276b4ed0dcf
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-04-11 15:07:11 +0200

    test

--- launio/settings.py
+++ launio/settings.py
@@ -26,7 +26,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'launio.herokuapp.com']
 # TEMPLATE_DEBUG = DEBUG
 
 # Application definition
