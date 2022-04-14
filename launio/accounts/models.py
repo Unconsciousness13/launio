8a07ef64b7f6789a6124eaebd20056e0972f664c
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-04-14 15:32:19 +0200

    adding some additional data

--- launio/accounts/models.py
+++ launio/accounts/models.py
@@ -14,30 +14,30 @@ IMAGE_MAX_SIZE_IN_MB = 2
 MIN_NAMES_LENGTH_VALIDATOR = 2
 
 
-# class CustomAccountManager(BaseUserManager):
-#
-#     def create_superuser(self, email, user_name, first_name, last_name, password, **other_fields):
-#         other_fields.setdefault('is_staff', True)
-#         other_fields.setdefault('is_superuser', True)
-#         other_fields.setdefault('is_active', True)
-#
-#         if other_fields.get('is_staff') is not True:
-#             raise ValueError('Superuser must be assigned to is_staff=True.')
-#
-#         if other_fields.get('is_superuser') is not True:
-#             raise ValueError('Superuser must be assigned to is_superuser=True.')
-#
-#         return self.create_user(email, user_name, first_name, last_name, password, **other_fields)
-#
-#     def create_user(self, email, user_name, first_name, last_name, password, **other_fields):
-#         if not email:
-#             raise ValueError(gettext_lazy('El email es obligatorio !'))
-#
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, user_name=user_name, first_name=first_name, last_name=last_name,
-#                           **other_fields)
-#         user.set_password(password)
-#         user.save()
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, first_name, last_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, user_name, first_name, last_name, password, **other_fields)
+
+    def create_user(self, email, user_name, first_name, last_name, password, **other_fields):
+        if not email:
+            raise ValueError(gettext_lazy('El email es obligatorio !'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, first_name=first_name, last_name=last_name,
+                          **other_fields)
+        user.set_password(password)
+        user.save()
 
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
@@ -62,7 +62,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    # objects = CustomAccountManager()
+    objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['user_name', 'first_name', 'last_name']
